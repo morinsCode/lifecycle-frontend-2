e0da1268c5c8d78cdd77f0ed34115c79a4b3878f
gapsofmind <morin.ibrahim@gmail.com>
2025-10-20 15:03:00 +0200

    deploy hook test

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>The New moesCode</h1>
+      <h1>The Newest moesCode</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
